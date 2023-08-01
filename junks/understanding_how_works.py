5272e5b26933c0c679e341c70c163a6316f373b9
DDadeA <jyh2eh@gmail.com>
2023-08-01 18:08:29 +0900

    add example.py

--- junks/understanding_how_works.py
+++
@@ -1,8 +0,0 @@
-import torch, whisper
-
-audio_model = whisper.load_model('medium')
-print(' > model loaded! ')
-
-result = audio_model.transcribe('test.wav', fp16=torch.cuda.is_available())
-text = result['text'].strip()
-print(text)
